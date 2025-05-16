1b3dc9f71f4b14229f16bd9b26a49b0fbc48e71b
Hugo Castillo <112522025+Hugotk64@users.noreply.github.com>
2025-05-16 19:15:21 +0000

    feat: adds function to load account from file.

--- main.c
+++ main.c
@@ -21,14 +21,38 @@ int main() {
     return 0;
 }
 
+// ***** File I/O Functions *****
+// Load accounts from file
+Account loadAccount(int accountNumber, int pin, int *found) {
+    // Open the file in read mode
+    FILE *file = fopen("accounts.txt", "r");
+    Account account;
+    *found = 0;
 
+    // Check if the file opened successfully
+    if (file == NULL) {
+        printf("Error opening file.\n");
+        return account;
+    }
+
+    // Read each line from the file and check for matching account number and pin
+    while (fscanf(file, "%49[^,],%d,%d,%f\n", account.name, &account.accountNumber, &account.pin, &account.balance) == 4) {
+        // Check if the account number and pin match
+        if (account.accountNumber == accountNumber && account.pin == pin) {
+            *found = 1;
+            break;
+        }
+    }
+
+    fclose(file);
+    return account;
+}
 
 // ***** Utility Functions *****
 // Clean terminal
 void cleanScreen() {
     printf("\033[H\033[J");
 }
-
 // Pause the program
 void pause() {
     printf("Press Enter to continue...");
