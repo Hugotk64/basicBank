c4cb7844a8540a391b60f158b996a6282f778775
Hugo Castillo <112522025+Hugotk64@users.noreply.github.com>
2025-05-16 19:08:00 +0000

    feat: Add utility functions to clean terminal and pause system.

--- main.c
+++ main.c
@@ -2,7 +2,35 @@
 #include <stdlib.h>
 #include <string.h>
 
+// Account structure
+typedef struct {
+    char name[50];
+    int accountNumber;
+    int pin;
+    float balance;
+} Account;
+
+// Fuction prototype
+
+// Utility functions
+void cleanScreen();
+void pause();
+
 int main() {
     printf("Hello, World!\n");
     return 0;
+}
+
+
+
+// ***** Utility Functions *****
+// Clean terminal
+void cleanScreen() {
+    printf("\033[H\033[J");
+}
+
+// Pause the program
+void pause() {
+    printf("Press Enter to continue...");
+    while (getchar() != '\n');
 }
